50326abf71718749fa78b93857b0c8136736baaf
llextor <bukhrinov@gmailcom>
2018-04-17 01:43:31 +0300

    menu

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import {RouterModule, Routes} from '@angular/router';
+import {UserComponent} from './user/user.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/user', pathMatch: 'full' },
+  { path: 'user', component: UserComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'contacts', component: ContactsComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HeaderComponent } from './header/header.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ContactsComponent,
+    UserComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
